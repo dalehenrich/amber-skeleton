e8ac9b6209d2c5fc67abf9267bd71a2e5805c530
Dale Henrichs <dhenrich@vmware.com>
2012-04-20 16:59:58 -0700

    rewrite AmberProjectImporter.js with most recent amber bugfixes

--- server/projects/importer/js/AmberProjectImporter.js
+++ server/projects/importer/js/AmberProjectImporter.js
@@ -2,6 +2,22 @@ smalltalk.addPackage('AmberProjectImporter', {});
 smalltalk.addClass('AmberProjectImporter', smalltalk.Object, [], 'AmberProjectImporter');
 
 smalltalk.addMethod(
+unescape('_get_onComplete_'),
+smalltalk.method({
+selector: unescape('get%3AonComplete%3A'),
+category: 'not yet classified',
+fn: function (ajaxUrl, completeBlock){
+var self=this;
+smalltalk.send((typeof jQuery == 'undefined' ? nil : jQuery), "_ajax_options_", [ajaxUrl, smalltalk.HashedCollection._fromPairs_([smalltalk.send("type", "__minus_gt", ["GET"]),smalltalk.send("complete", "__minus_gt", [(function(jqXHR, textStatus){return smalltalk.send(completeBlock, "_value_value_", [jqXHR, textStatus]);})])])]);
+return self;},
+args: ["ajaxUrl", "completeBlock"],
+source: unescape('get%3A%20ajaxUrl%20onComplete%3A%20completeBlock%0A%09jQuery%20%0A%09%09ajax%3A%20ajaxUrl%0A%20%20%20%20%20%20%20%20%09options%3A%20%23%7B%0A%09%09%09%27type%27%20-%3E%20%27GET%27.%0A%20%20%20%20%09%09%09%27complete%27%20-%3E%20%5B%20%3AjqXHR%20%3AtextStatus%20%7C%20%20completeBlock%20value%3A%20jqXHR%20value%3A%20textStatus%5D%0A%09%09%7D'),
+messageSends: ["ajax:options:", unescape("-%3E"), "value:value:"],
+referencedClasses: []
+}),
+smalltalk.AmberProjectImporter.klass);
+
+smalltalk.addMethod(
 unescape('_importSTPackage_prefix_'),
 smalltalk.method({
 selector: unescape('importSTPackage%3Aprefix%3A'),
@@ -38,22 +54,6 @@ args: ["packageNameList", "prefix"],
 source: unescape('importSTPackages%3A%20packageNameList%20prefix%3A%20prefix%0A%09%22AmberProjectImporter%0A%09%09importSTPackages%3A%20%23%28%27AmberProjectPage%27%29%0A%09%09prefix%3A%20%27st/%27%22%0A%0A%09%7C%20loadBlock%20%7C%0A%09loadBlock%20%3A%3D%20%5B%3Aindex%20%7C%20%7C%20packageName%20next%20%7C%0A%09%09packageName%20%3A%3D%20packageNameList%20at%3A%20index.%0ATranscript%20cr%3B%20show%3A%20%27import%20%27%2C%20packageName.%0A%09%09self%20get%3A%20prefix%2C%20packageName%2C%20%27.st%27%20%20onComplete%3A%20%5B%3AjqXHR%20%3AtextStatus%20%7C%20%0A%09%09%09jqXHR%20readyState%20%3D%204%20ifTrue%3A%20%5B%20%7C%20chunks%20%7C%0A%09%09%09%09Importer%20new%20import%3A%20jqXHR%20responseText%20readStream.%0A%09%09%09%09next%20%3A%3D%20index%20+%201.%0A%09%09%09%09next%20%3C%3D%20packageNameList%20size%0A%09%09%09%09%09ifTrue%3A%20%5B%20loadBlock%20value%3A%20next%20%5D%20%20%5D%5D%5D.%0A%09packageNameList%20size%20%3E%3D%201%20%0A%09%09ifTrue%3A%20%5B%20loadBlock%20value%3A%201%20%5D%0A'),
 messageSends: ["at:", "cr", "show:", unescape("%2C"), "get:onComplete:", "ifTrue:", unescape("%3D"), "readyState", "import:", "new", "readStream", "responseText", unescape("+"), unescape("%3C%3D"), "size", "value:", unescape("%3E%3D")],
 referencedClasses: ["Transcript", "Importer"]
-}),
-smalltalk.AmberProjectImporter.klass);
-
-smalltalk.addMethod(
-unescape('_get_onComplete_'),
-smalltalk.method({
-selector: unescape('get%3AonComplete%3A'),
-category: 'not yet classified',
-fn: function (ajaxUrl, completeBlock){
-var self=this;
-smalltalk.send((typeof jQuery == 'undefined' ? nil : jQuery), "_ajax_options_", [ajaxUrl, smalltalk.HashedCollection._fromPairs_([smalltalk.send("type", "__minus_gt", ["GET"]),smalltalk.send("complete", "__minus_gt", [(function(jqXHR, textStatus){return smalltalk.send(completeBlock, "_value_value_", [jqXHR, textStatus]);})])])]);
-return self;},
-args: ["ajaxUrl", "completeBlock"],
-source: unescape('get%3A%20ajaxUrl%20onComplete%3A%20completeBlock%0A%09jQuery%20%0A%09%09ajax%3A%20ajaxUrl%0A%20%20%20%20%20%20%20%20%09options%3A%20%23%7B%0A%09%09%09%27type%27%20-%3E%20%27GET%27.%0A%20%20%20%20%09%09%09%27complete%27%20-%3E%20%5B%20%3AjqXHR%20%3AtextStatus%20%7C%20%20completeBlock%20value%3A%20jqXHR%20value%3A%20textStatus%5D%0A%09%09%7D'),
-messageSends: ["ajax:options:", unescape("-%3E"), "value:value:"],
-referencedClasses: []
 }),
 smalltalk.AmberProjectImporter.klass);
 
